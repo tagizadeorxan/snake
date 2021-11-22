34fb4a7ba4298b28f4d8e0856c6c2302a5ae9b80
Orhan Taghizadeh <tagizade.orxan@gmail.com>
2021-11-22 12:50:35 +0400

    initial

---
+++ ReactNativeGame/components/Food/index.js
@@ -0,0 +1,18 @@
+// commponets/Food/index.js
+import React from 'react';
+import {View} from 'react-native';
+export default function Food({position, size}) {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor: 'green',
+        position: 'absolute',
+        left: position[0] * size,
+        top: position[1] * size,
+        borderRadius: 50,
+      }}
+    />
+  );
+}
